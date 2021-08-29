7d617317d8d4b0068aa49f5d99dc6a8fea27ee9f
Bopp, Florian <florian.bopp@haw-hamburg.de>
2021-08-29 18:51:23 +0200

    initialized the vue project with simple camera compnent to show the device webcam

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Camera />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Camera from './components/Camera'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Camera
+
   }
 }
 </script>
+
 
 <style>
 #app {
---
+++ src/components/Camera.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="camera">
+        <video autoplay class="feed"></video>
+    </div>
+</template>
+
+<script>
+// import * as tf from '@tensorflow/tfjs'
+
+export default {
+    name: "camera",
+    data: function() {
+        return {
+            cameraConstraints: {
+                video: {
+                    frameRate: 240
+                }
+            }
+        }
+    },
+    methods: {
+        init () {
+            if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
+                navigator.mediaDevices.getUserMedia(this.cameraConstraints)
+                    .then(stream => {
+                        const videoPlayer = document.querySelector("video");
+                        videoPlayer.srcObject = stream;
+                        videoPlayer.play();
+                    })
+            }
+        }
+    },
+    beforeMount(){
+        this.init()
+    },
+    created() {
+        // const cocoSSD = require('@tensorflow-models/coco-ssd');
+
+        // (async ())
+    }
+
+}
+</script>
+
+<style>
+    .feed {
+        position: fixed;
+        right: 0;
+        top: 0;
+        /* min-width: 100%;
+        min-height: 100%; */
+    }
+
+</style>
