a26cfc32c27e54d24363a3e695ed71f7f623c878
Bopp, Florian <florian.bopp@haw-hamburg.de>
2021-09-02 19:52:48 +0200

    added another button to switch the camera if available

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="camera">
-        <video autoplay playsinline class="feed"></video>
-        <button id="infoBtn" @click="printInfo()">Print Info</button>
-    </div>
+  <div class="camera">
+    <video autoplay playsinline class="feed"></video>
+    <button id="infoBtn" @click="printInfo()">Print Info</button>
+    <button id="switchCam" @click="switchCamera()">Switch Camera</button>
+  </div>
 </template>
 
 <script>
@@ -10,67 +11,94 @@
 // import * as mobilenet from '@tensorflow-models/mobilenet'
 
 export default {
-    name: "camera",
-    data: function() {
-        return {
-            currentPrediciton : {},
-            stream: null,
-            cameraConstraints: {
-                video: {
-                    frameRate: { 
-                        min: 30,
-                        ideal: 120,
-                        },
-                    facingMode: "environment",
-                    width: { ideal: 1920 },
-                    height: { ideal: 1200 }
-                }
-            }
-        }
-    },
-    methods: {
-        async init(){
-            if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
-                console.log("Getting camera from user")
-                this.stream = await navigator.mediaDevices.getUserMedia(this.cameraConstraints)
-                const videoPlayer = document.querySelector("video");
-                videoPlayer.srcObject = this.stream;
-                videoPlayer.play();
-                }
-
+  name: "camera",
+  data: function() {
+    return {
+      currentPrediciton: {},
+      stream: null,
+      mediaTrack: null,
+      cameraSelector: "environment",
+      cameraConstraints: {
+        video: {
+          frameRate: {
+            min: 30,
+            ideal: 120,
+          },
+          facingMode: this.cameraSelector,
+          width: { ideal: 1920 },
+          height: { ideal: 1200 },
         },
-        printInfo (){
-            console.log(this.stream.getVideoTracks()[0].getSettings())
-        }
-                
-
+      },
+    };
+  },
+  methods: {
+    async init() {
+      if (
+        "mediaDevices" in navigator &&
+        "getUserMedia" in navigator.mediaDevices
+      ) {
+        console.log("Getting camera from user");
+        this.stream = await navigator.mediaDevices.getUserMedia(
+          this.cameraConstraints
+        );
+        this.mediaTrack = await this.stream.getVideoTracks()[0];
+        const videoPlayer = document.querySelector("video");
+        videoPlayer.srcObject = this.stream;
+        videoPlayer.play();
+      }
     },
-    beforeMount(){
-        this.init()
+    printInfo() {
+      console.log(this.mediaTrack.getSettings());
     },
-    created() {
-        // const cocoSSD = require('@tensorflow-models/coco-ssd');
-
-        // (async ())
+    switchCamera(){
+      console.log("switching camera")
+      this.cameraSelector = this.cameraSelector === "environment" ? "user" : "environment";
+      this.mediaTrack.applyConstraints(this.cameraConstraints)
     }
-
-}
+  },
+  beforeMount() {
+    this.init();
+  },
+  created() {
+    // const cocoSSD = require('@tensorflow-models/coco-ssd');
+    // (async ())
+  },
+};
 </script>
 
 <style scoped>
+.camera {
+  position: absolute;
+  top: 0;
+  left: 0;
 
-    .feed {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        max-height: 100vh;
-    }
-    #infoBtn {
-        position: absolute;
-        z-index: 1;
-        width: 10vw;
-        height: 5vh;
-        transform: translate(-50%);
-    }
+}
+.feed {
+  top: 0;
+  left: 0;
+  width: 100vw;
+  max-height: 100vh;
+  z-index: 0;
+}
+button {
+  position: absolute;
+  top: 90%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  background-color: #555;
+  color: white;
+  font-size: 16px;
+  padding: 12px 24px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+#infoBtn {
+
+  left: 40%;
+}
+#switchCam {
+  left: 60%;
+}
 </style>
